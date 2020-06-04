c681a9d9448a4a9b84ae465b4795ff2180cd82fd
Glitch (organized-narrow-queen) <none>
2020-06-04 18:28:12 +0000

    🚧👦 Checkpoint ./Fury.js:13216602/234

--- Fury.js
+++ Fury.js
@@ -130,7 +130,7 @@ const ytdl = require("ytdl-core");
 const getYoutubeID = require("get-youtube-id");
 const fetchVideoInfo = require("youtube-info");
 const YouTube = require("simple-youtube-api");
-const youtube = new YouTube("AIzaSyCduyy4HvC8jTEbgPdrLr72XnPnV0upqsw");
+const youtube = new YouTube("AIzaSyBDsBs0_FMfU4wvZlrHgs9oOHJ14s6CF8c");
 
 let active = new Map();
 
